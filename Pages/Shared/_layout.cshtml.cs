af6043a40d7923fc5474169fb793d0892e6bd6aa
Guilherme Tomaz <guilhermetomaz.bsi@gmail.com>
2025-04-08 20:29:39 -0300

    Feat: Páges in Razor, first commit front-end

---
+++ Pages/Shared/_layout.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CRMApplicationWeb.Pages.Shared
+{
+    public class _layoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/wwwroot/css/site.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CRMApplicationWeb.Pages.wwwroot.css
+{
+    public class siteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
